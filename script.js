4c7bc9a45ccca7e414660fc8f8ef89cd040a9771
prerana2601 <155650129+prerana2601@users.noreply.github.com>
2024-08-07 11:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
+// script.js
+
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+    // Define Roman numeral symbols and their corresponding values
+    const romanSymbols = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
 
-  //your code here
+    // Initialize the result string
+    let result = '';
 
+    // Iterate over the romanSymbols array
+    for (let i = 0; i < romanSymbols.length; i++) {
+        const [symbol, value] = romanSymbols[i];
+
+        // Determine the number of times the symbol should be used
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+function convertNumber() {
+    // Get the user input
+    const inputNumber = parseInt(document.getElementById('numberInput').value, 10);
+    const resultElement = document.getElementById('result');
 
+    // Check for valid input
+    if (isNaN(inputNumber) || inputNumber < 0 || inputNumber > 100000) {
+        resultElement.textContent = 'Please enter a number between 0 and 100000.';
+        return;
+    }
 
+    // Compute the Roman numeral
+    const result = convertToRoman(inputNumber);
 
-
-// do not edit below this line
-module.exports = convertToRoman
+    // Display the result
+    resultElement.textContent = result;
+}
